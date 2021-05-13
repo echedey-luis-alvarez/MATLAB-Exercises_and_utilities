174cfea9770ffc2809d57db6cfa8a54825d85541
echedey-luis-alvarez <80125792+echedey-luis-alvarez@users.noreply.github.com>
2021-05-13 21:23:11 +0200

    Bugfix: Simpson is better than Trapezium
    Rearrange some code, clarification, better output format

--- Integration/Tests_formulas_trapecio_y_simpson.m
+++ Integration/Tests_formulas_trapecio_y_simpson.m
@@ -8,7 +8,14 @@ clear, clc;
 format long g;
 
 % Runtime config variables
-nodes = 1e4;
+% What vars you use is up to you, but take into account the algorithm is made to fit a certain number of nodes.
+% Number of intervals being used
+subs_trapezium = 1e6;
+subs_simpson = 8e2;
+% Number of nodes being used
+nodes_tr = subs_trapezium + 1;
+nodes_sp = subs_simpson + 1;
+
 calculate_single_node_number = true;
   % True: calculates just with the given number of nodes (recommended)
   % False: calculates for nodes = 2:[1 or 2]:nodes (not recommended, uses up too much memory)
@@ -18,40 +25,44 @@ show_sols = false;
 % Función a integrar
 f = @(x) 1 ./ (1 + x .^ 2);
 % Extremos integración
-a = 3; b = 7;
+a = 0; b = 4.06;
 % Cuyo valor exacto es F = arctan(x), la evaluamos entre 3 y 7
 true_value = atan(b) - atan(a);
 % Por tanto los métodos deben converger a true_value = 0.1798534997924783
+
+% Show config
+fprintf("Calculating integral of:\n");
+disp(f);
+fprintf("Evaluated between %G and %G\n", a, b);
+
+% Checks and user-proofing
+if ( nodes_tr < 2 )
+  error("Nodes must be greater or equal than 2 to apply Trapezium");
+end
+if ( nodes_sp < 3 )
+  error("Nodes must be greater or equal than 3 to apply Simpson");
+end
 
 % Runtime variable alocation
 best_trapezium = 0;
 best_simpson = 0;
 
 if (~calculate_single_node_number)
-  sols_trapezium = NaN(nodes-1, 2);
-  sols_simpson = NaN(nodes/2-1, 2);
+  sols_trapezium = NaN(nodes_tr-1, 2);
+  sols_simpson = NaN(nodes_sp/2-1, 2);
 end
 
-% Show config
-fprintf("Calculating integral of:\n");
-disp(f);
-fprintf("Evaluated between %G and %G\n\n", a, b);
-
-% Checks and user-proofing
-if ( nodes < 2 )
-  error("Nodes must be greater or equal than 2");
-end
 
 % Trapezium
-fprintf("Trapezium: start of calculations");
+fprintf("\nTrapezium: start of calculations\n");
 if (calculate_single_node_number)
-  x = linspace(a, b, nodes);
+  x = linspace(a, b, nodes_tr);
   y = f(x);
 
   best_trapezium = formula_trapezium_composite(x(2) - x(1), y);
 
 else
-  for i = 2:nodes
+  for i = 2:nodes_tr
 
     x = linspace(a, b, i);
     y = f(x);
@@ -67,16 +78,18 @@ else
   end % !for
   best_trapezium = sols_trapezium(end);
 end % !if
-fprintf("Trapezium with %G nodes: %G\n", nodes, best_trapezium);
-fprintf("Trapezium: end of calculations");
+fprintf("Trapezium with %G nodes: %.10G\n", nodes_tr, best_trapezium);
+fprintf("Trapezium: end of calculations\n");
 
 % Simpson
-fprintf("Simpson: start of calculations");
+fprintf("\nSimpson: start of calculations\n");
+
+nodes_simpson = nodes_sp - mod(nodes_sp - 1, 2);
+if (mod(nodes_sp - 1, 2) ~= 0)
+  fprintf("Warning: not using a multiple-of-2 number of subintervals, now using %G nodes to execute Simpson properly\n", nodes_simpson );
+end
+
 if (calculate_single_node_number)
-  nodes_simpson = nodes - mod(nodes, 2);
-  if (mod(nodes, 2) ~= 0)
-    fprintf("Warning: not using a multiple-of-2 number of nodes, now using %G nodes to execute Simpson properly", nodes_simpson );
-  end
 
   x = linspace(a, b, nodes_simpson);
   y = f(x);
@@ -84,14 +97,14 @@ if (calculate_single_node_number)
   best_simpson = formula_simpson_composite(x(2) - x(1), y);
 
 else
-  for i = 2:nodes/2
+  for i = 2:2:nodes_simpson
 
-    x = linspace(a, b, i*2);
+    x = linspace(a, b, i);
     y = f(x);
 
     valor = formula_simpson_composite(x(2) - x(1), y);
 
-    sols_simpson(i-1, :) = [i*2, valor];
+    sols_simpson(i/2, :) = [i, valor];
 
     if (show_sols)
       fprintf(sols_simpson);
@@ -100,26 +113,21 @@ else
   end % !for
   best_simpson = sols_simpson(end);
 end % !if
-fprintf("Simpson with %G nodes: %G\n", nodes_simpson, best_simpson);
-fprintf("Simpson: end of calculations");
+fprintf("Simpson with %G nodes: %.10G\n", nodes_simpson, best_simpson);
+fprintf("Simpson: end of calculations\n");
 
 
 % Show errors
 fprintf("\nErrors:\n");
-fprintf("Err(Trapezium) = %G [%G nodes]\n", true_value - best_trapezium, nodes);
-fprintf("Err(Simpson) = %G [%G nodes]\n", true_value - best_simpson, nodes_simpson);
+fprintf("Err(Trapezium) = %.10G [%G nodes]\n", true_value - best_trapezium, nodes_tr);
+fprintf("Err(Simpson) = %.10G [%G nodes]\n", true_value - best_simpson, nodes_simpson);
 
-% Errors (with 1E+04 nodes):
-% Err(Trapezium) = -7.25529E-10
-% Err(Simpson) = 8.00155E-06
+% Errors:
+% Err(Trapezium) = 3.648925873E-08 [1001 nodes]
+% Err(Simpson) = 3.899103262E-13 [501 nodes]
 
-% Errors (with 1E+05 nodes):
-% Err(Trapezium) = -6.56952E-12
-% Err(Simpson) = 8.00016E-07
+% Errors:
+% Err(Trapezium) = 6.550315845E-14 [1E+06 nodes]
+% Err(Simpson) = 5.86197757E-14 [801 nodes]
 
-% Errors (with 1E+07 nodes):
-% Err(Trapezium) = -9.06071E-11
-% Err(Simpson) = 7.90936E-09
-
-% Hay veces en las que la vida te sorprende;
-% pero que la fórmula del trapecio funcione mejor que la de Simpson es otra cosa
+% Simpson is the boss
