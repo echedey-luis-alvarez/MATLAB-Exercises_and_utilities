b7699d97ea25725611e678e6af5594143d0e1e2e
echedey-luis-alvarez <80125792+echedey-luis-alvarez@users.noreply.github.com>
2021-05-21 19:14:01 +0200

    Añadir comentarios y explicar argumentos

--- Diff eq/euler.m
+++ Diff eq/euler.m
@@ -1,9 +1,25 @@
+% Autor: Echedey Luis Álvarez
+% Fecha: 21/05/2021
+% 
+% Aplicación general de aproximación de ec. diferenciales por el método de Euler
+
 function E = euler( f, a, b, y_a, M )
+%{
+    Args:
+      f: 2-vars anonym function which represents explicitly the derivative of y
+      a & b: begin and end of interval of aproximation
+      y_a: known value of f, f(a)
+      M: desired number of iterations
+    Output:
+      E: (M+1) x 3 matrix
+        E(:,1): index column (first one is 0)
+        E(:,2): independent value (t_k, x_k)
+        E(:,3): dependent value (y_k)
+%}
     h = (b - a) ./ M; disp(h);
     E = NaN( [M+1, 3] );
     E(1, :) = [0, a, y_a];
     for j=1:M
-       % y(j+1)=y(j)+h*f(t(j),y(j));
        E(j+1, :) = [ j, a + j .* h, E(j, 3) + h * f( E(j, 2), E(j, 3) ) ];
     end % !for
 end % !if
